d6a7f4f2c3e6e70530114dc12dc670378c4d5fe3
ittySparrow <ittysparrow@gmail.com>
2020-10-14 18:43:42 +1000

    minor css changes

--- src/OrdersPage/OrdersPage.jsx
+++ src/OrdersPage/OrdersPage.jsx
@@ -3,27 +3,30 @@ import _ from "lodash";
 import "./OrdersPage.css";
 
 export default ({ orders }) => {
-  if (!orders) {
-    return <>There were no orders yet</>;
-  }
   return (
-    <table>
-      <thead>
-        <tr>
-          {_.keys(orders[0]).map((name) => (
-            <th key={Math.floor(Math.random() * 1000)}>{name}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((order) => (
-          <tr key={Math.floor(Math.random() * 1000)}>
-            {_.values(order).map((value) => (
-              <td key={Math.floor(Math.random() * 1000)}>{value}</td>
+    <div className="orders-page-wrapper">
+      {!orders ? (
+        <div className="no-orders">There were no orders yet</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              {_.keys(orders[0]).map((name) => (
+                <th key={Math.floor(Math.random() * 1000)}>{name}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={Math.floor(Math.random() * 1000)}>
+                {_.values(order).map((value) => (
+                  <td key={Math.floor(Math.random() * 1000)}>{value}</td>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
