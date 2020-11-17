0adc6274875df1256b043d84f6f8c5de43d2e70d
ittySparrow <ittysparrow@gmail.com>
2020-11-17 16:43:31 +1000

    combine error messages into obj

--- src/RegisterPage/RegisterForm/RegisterFormComponents/DateSelect.jsx
+++ src/RegisterPage/RegisterForm/RegisterFormComponents/DateSelect.jsx
@@ -1,30 +1,28 @@
-import React from "react";
-import _ from "lodash";
-import { errorMessageDateInput } from "../../../_validator/errorMessages";
-import CustomSelect from "./CustomSelect";
-import { formatDate } from "../../../_utils/format";
-import { useFormContext } from "react-hook-form";
-import resetFields from "../../../_utils/resetFields";
+import React from 'react'
+import _ from 'lodash'
+import CustomSelect from './CustomSelect'
+import { formatDate } from '../../../_utils/format'
+import { useFormContext } from 'react-hook-form'
+import resetFields from '../../../_utils/resetFields'
 
 export default ({ dates, showDates }) => {
-  const { reset, getValues } = useFormContext();
+  const { reset, getValues } = useFormContext()
 
-  const resetTime = resetFields("date", { time: "" }, reset, getValues);
+  const resetTime = resetFields('date', { time: '' }, reset, getValues)
 
   const options = showDates
     ? _.keys(dates).map((date) => ({
         key: date,
         value: formatDate(date),
       }))
-    : [];
+    : []
 
   return (
     <CustomSelect
       options={options}
-      name="date"
-      placeholder={"Дата"}
+      name='date'
+      placeholder={'Дата'}
       handleChange={resetTime}
-      errorMessageInput={errorMessageDateInput}
     />
-  );
-};
+  )
+}
