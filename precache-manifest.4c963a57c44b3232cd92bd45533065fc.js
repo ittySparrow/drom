9b64b8d0031e0e2204a0c3eb66159cbcae24d20c
ittySparrow <ittysparrow@gmail.com>
2021-01-10 21:06:57 +1000

    deploy

---
+++ precache-manifest.4c963a57c44b3232cd92bd45533065fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e761d6425443dba8500189864dd5fb9a",
+    "url": "/drom/index.html"
+  },
+  {
+    "revision": "80f5aa5365f3b0e701a0",
+    "url": "/drom/static/css/main.c7b3e27b.chunk.css"
+  },
+  {
+    "revision": "1c347e7d194bdd76ead3",
+    "url": "/drom/static/js/2.13509ff3.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/drom/static/js/2.13509ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f5aa5365f3b0e701a0",
+    "url": "/drom/static/js/main.e0214b2f.chunk.js"
+  },
+  {
+    "revision": "ef593dd260c610cc10bd",
+    "url": "/drom/static/js/runtime-main.012fc1c6.js"
+  },
+  {
+    "revision": "2f466207a1a285a46c74d0fd79dfd04a",
+    "url": "/drom/static/media/logo.2f466207.svg"
+  }
+]);
